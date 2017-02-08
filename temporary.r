ecb1a58a15c8188513080961ea875f3b4b4bc73a
YD Hwang <ydhwang.ad@gmail.com>
2017-02-08 11:58:19 -0500

    added rmse() function.

--- temporary.r
+++ temporary.r
@@ -52,3 +52,15 @@ lsos <- function(..., n=10) {
 }
 
 lsos()	
+
+rmse <- function(x,y){
+# root mean square error function
+# x, y can be vector, matrix or array but their sizes should match
+# returns a scalar 
+	if (any(dim(x)!=dim(y))){stop()} # check function to prevent disastrous cases
+	sqrt(mean((x-y)^2))
+
+}
+
+
+
